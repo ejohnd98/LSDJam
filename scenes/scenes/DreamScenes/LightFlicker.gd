95d7d92c6fea7961c198bdc600ebd80be47a91cd
Eric Dahl <ejohnd98@gmail.com>
2024-02-11 04:10:09 -0800

    Randomized lightflicker start

--- scenes/scenes/DreamScenes/LightFlicker.gd
+++ scenes/scenes/DreamScenes/LightFlicker.gd
@@ -6,7 +6,14 @@ extends OmniLight3D
 @export var light_constant = 0.5
 var time_passed = 0.0
 
+func _ready():
+	time_passed = randf()
+	speed -= randf()
+
 func _process(delta):
 	time_passed += delta * speed
 	var noise_sample = noise_texture.noise.get_noise_1d(time_passed)
 	light_energy = light_constant + abs(noise_sample) * light_scale
+	
+	if time_passed > 100:
+		time_passed = 0.0
